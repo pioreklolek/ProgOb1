a9d9416e3ef5fc9c804a48fc79cd9ddd737970bf
pioreklolek <piotrorlowski2004@gmail.com>
2024-03-20 17:24:07 +0100

    test

--- src/Polygon.java
+++ src/Polygon.java
@@ -1,24 +1,33 @@
+import java.io.StringReader;
+
 public class Polygon {
     private Point[] points;
+    private Style style;
+
+    public Polygon(Point[] points, Style style) {
+        this.points = points;
+        this.style = style;
+    }
 
     public Polygon(Point[] points) {
         this.points = points;
+        this.style = new Style("none","black", 1.0);
     }
-
 
     public String toSvg() {
         StringBuilder stringBuilder = new StringBuilder();
         stringBuilder.append("<polygon points=\"");
         for (Point point : points) {
-            stringBuilder.append(point.x).append(", ").append(point.y).append(" ");
+            stringBuilder.append(point.x).append(",").append(point.y).append(" ");
         }
-        stringBuilder.append("\" style=\"fill:none;stroke:black;stroke-width:1\"/>\n");
+        stringBuilder.append(style.toSvg()).append("/>");
         return stringBuilder.toString();
     }
-    public Polygon(Polygon src){
+
+    public Polygon(Polygon src) {
         this.points = new Point[src.points.length];
-        for(int i=0; i < points.length; i++){
-            this.points[i] = new Point(src.points[i].x, src.points[i].y);
+        for (int i = 0; i < points.length; ++i) {
+            points[i] = new Point(src.points[i].x, src.points[i].y);
         }
     }
 }
